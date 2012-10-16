d8db178f6519e58e24ed7ecad83ed4800ec26e67
tpawel <tpawel@student.agh.edu.pl>
2012-10-16 21:31:45 +0200

    menu do wyboru funkcji, podawanie argumentow w pliku

---
+++ main.m
@@ -0,0 +1,33 @@
+load neuron.conf;
+
+activation = menu("Wybierz funkcje aktywacji","Liniowa","Skokowa","Sigmoidalna");
+
+if (activation==1)
+   a=1;
+   b=0;
+   activationFunction = @(X) linear(X,a,b);
+elseif (activation==2)
+   a=7;
+   activationFunction = @(X) threshold(X,a);
+elseif (activation==3)
+   activationFunction = @sigmoid;
+endif
+
+thetas = {}
+if (random_weights>0)
+   input_cols = size(input_data,2)
+   cols = input_cols
+   for i=1:size(layer_neuron,1)
+	thetas{i}=rand(layer_neuron(i,1),cols+1);
+	cols=size(thetas{i}, 1);
+   end
+else
+   thetas = weights;
+endif
+
+
+
+
+nn(activationFunction,thetas,input_data,results);
+
+
