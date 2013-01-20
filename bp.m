0d75a919698b4d30b627f251a23621a8ef619a09
tpawel <tpawel@student.agh.edu.pl>
2013-01-20 19:45:13 +0100

    poprawki do bp, nie ma jeszcze biasu

--- bp.m
+++ bp.m
@@ -16,9 +16,19 @@ function result=bp(input_data,expected,step)
   endfor
 
   result=answers;
-
+ 
   %if learn poprawic wagi
-
-  layers{1}.weights = layers{1}.weights + sigmoidDerivative([layer.bias*ones(input_rows,1) input_data]*layer.weights')*(expected-answers)*[layer.bias*ones(input_rows,1) input_data];
+%
+%  expected_row = expected(1,1:end)
+%  answers_row = answers(1,1:end)
+  eta = 0.6;
+%  delta_w = eta * ((expected_row-answers_row) .* (1-answers_row).* answers_row) * input_data
+%  layers{1}.weights(1,1:end) = layers{1}.weights(1,1:end) + [0 delta_w];
+%  expected
+%  answers
+%  input_data
+  deltas = eta * ((expected-answers) .* (1-answers) .* answers) * input_data
+  delta_bias = 0;
+  layers{1}.weights = layers{1}.weights +  [delta_bias*ones(input_rows,1) deltas];
 
 endfunction
