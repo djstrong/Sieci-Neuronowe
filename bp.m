8079b44d196e6dba058c324088893a874ce4e87a
djstrong <djstrong@gmail.com>
2013-01-21 21:33:14 +0100

    niby dziala

--- bp.m
+++ bp.m
@@ -3,7 +3,47 @@ function result=bp(input_data,expected,step)
   input_rows = size(input_data, 1);
 
   answers=input_data;
+eta = 0.6;
+  for i=1:input_rows
+    input = input_data(i,:);
+    output = expected(i,:);
 
+    answers=input;
+    a{1}=input;
+    for j=1:length(layers)
+      layer = layers{j};            
+      answers = layer.activation_function([layer.bias*ones(1,1) answers]*layer.weights');
+      a{j+1}=answers;
+    endfor
+
+    delta = {};
+    j=length(layers);
+    delta{j} = ((1-a{j+1}) .* a{j+1}) .* (output-a{j+1});
+    layers{j}.weights = layers{j}.weights+eta * delta{j}' *[1 a{j}];
+
+
+    for j=length(layers)-1:-1:1
+      delta{j} = layers{j+1}.weights.*delta{j+1}'; %?
+    endfor
+
+    for j=1:length(layers)-1
+%  j
+      layer = layers{j};
+%delta{j}
+       %layer.weights
+%((1-a{j+1}) .* a{j+1})
+%a{j}
+%delta{j}
+%delta{j}(:,2:end)
+sum(delta{j}(:,2:end))
+      layer.weights = layer.weights+eta* (sum(delta{j}(:,2:end)) .* ((1-a{j+1}) .* a{j+1}))' *[1 a{j}];
+    endfor
+    a
+
+  endfor
+
+
+  return
   a={}
   a{1}=input_data;
 
@@ -31,7 +71,7 @@ function result=bp(input_data,expected,step)
 %  expected
 %  answers
 %  input_data
-
+a
   gradient = ((expected-a{length(layers)+1}) .* (1-a{length(layers)+1}) .* a{length(layers)+1})
   deltas = eta * gradient' * a{length(layers)}%input_data
   delta_bias = eta * gradient' * ones(input_rows,1)
@@ -39,12 +79,13 @@ function result=bp(input_data,expected,step)
 
   %expected = layers{length(layers)}.weights .* [delta_bias deltas]
 
-
+  del = expected-a{length(layers)+1}
+  
 
   for i=length(layers)-1:-1:1
     %mysle, ze mozna posumowac gradient kolumnami, czyli gradient= sum(gradient, kolumnami). Powinno to ulatwic sprawe
     %g = ∑ mi * ei ... (for i=1 to r)
-    g = gradient*layers{i+1}.weights
+    g = del*layers{i+1}.weights
     %e = z * (1 - z) * g ... Error at the hidden layer neuron
     e = [ones(size(a{i+1},1), 1) (a{i+1} .* (1-a{i+1}))] .* g
     %Δθ = λ * e ... The change in θ
